b45bbbeead72eae90cf7d4e2b3832261d6d80121
Eduardo S. Morbeck <eduardomorbeck187@gmail.com>
2024-06-12 10:31:25 -0300

    update

--- script.js
+++ script.js
@@ -27,12 +27,40 @@ function fuja(){
         music.src = 'img/Mantis Lords.mp3';
         music.play();
     }
+
+    if(count == 5){
+        titulo.classList.toggle('active');
+        txt.innerHTML = 'Desiste, não tem como clicar no "Não" ksksks';
+    }
+
+    if(count == 10){
+        txt.innerHTML = 'É sério...';
+    }
+
+    if(count == 15){
+        txt.innerHTML = 'Então tá bom, pode continuar...';
+    }
+
+    if(count == 20){
+        txt.innerHTML = 'Mds que persistente mano kkkkk';
+    }
+
+    if(count == 25){
+        txt.innerHTML = 'Tenta mais umas 5 vezes que eu te conto um segredo...';
+    }
+
+    if(count == 30){
+        txt.innerHTML = 'Mas tem que clicar no "Sim" pra descobrir...';
+    }
 }
 
 function clicou(){
+    if(count == 0){
+        titulo.classList.toggle('active');
+    }
+
     painel.style.backgroundImage = `url(img/hollowKnight-Hornet.png)`;
     btn.classList.toggle('active');
-    titulo.classList.toggle('active');
     txt.innerHTML = 'Resposta certa kkkkkk, eu te amo ;)';
     music.src = 'img/Hollow Knight (Title Theme).mp3';
     music.play();
